f39bad44a68a26129a2696ca3899e755e30b9f88
Emojigit <55009343+Emojigit@users.noreply.github.com>
2022-05-22 12:09:34 +0800

    link to user

--- __init__.py
+++ __init__.py
@@ -1,5 +1,7 @@
+# -*- coding: utf-8 -*-
+
 __author__ = "Emoji"
-__version__ = "1.0.0"
+__version__ = "1.0.1"
 __url__ = "https://github.com/Emojigit/userid"
 __description__ = "Get ids of user, message, etc."
 __dname__ = "userid"
@@ -9,11 +11,10 @@ def setup(bot):
     @bot.on(events.NewMessage(pattern='/userid'))
     async def start(event):
         resp = []
-        sender = await event.get_sender()
-        resp.append("Your user ID: `{}`".format(sender.id))
+        resp.append("Your user ID: `{}` [🔗](tg://user?id={})".format(event.sender_id,event.sender_id))
         rep = await event.get_reply_message()
         if rep != None:
-            resp.append("Replied message sender ID: `{}`".format(rep.sender.id))
+            resp.append("Replied message sender ID: `{}` [🔗](tg://user?id={})".format(rep.sender.id,rep.sender.id))
         resp = "\n".join(resp)
         await event.respond(resp)
         raise events.StopPropagation
