9ff8a858eeb5ef7996dbadd95f7f364fb094f155
Emojigit <55009343+Emojigit@users.noreply.github.com>
2022-05-22 12:29:08 +0800

    chatid

--- __init__.py
+++ __init__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 __author__ = "Emoji"
-__version__ = "1.0.2"
+__version__ = "1.0.3"
 __url__ = "https://github.com/Emojigit/userid"
 __description__ = "Get ids of user, message, etc."
 __dname__ = "userid"
@@ -9,7 +9,7 @@ __dname__ = "userid"
 from telethon import events
 def setup(bot):
     @bot.on(events.NewMessage(pattern='/userid'))
-    async def start(event):
+    async def userid(event):
         resp = []
         resp.append("Your user ID: `{}` [🔗](tg://user?id={})".format(event.sender_id,event.sender_id))
         rep = await event.get_reply_message()
@@ -20,8 +20,7 @@ def setup(bot):
         raise events.StopPropagation
 
     @bot.on(events.NewMessage(pattern='/uidlink'))
-    async def start(event):
-
+    async def uidlink(event):
         try:
             i = int(event.text.split(" ",1)[1])
             if i < 1: # -xxxxx is channel or group!
@@ -33,3 +32,9 @@ def setup(bot):
         else:
             await event.respond("[{id}](tg://user?id={id})".format(id=i))
         raise events.StopPropagation
+
+    @bot.on(events.NewMessage(pattern='/chatid'))
+    async def chatid(event):
+        await event.respond("Chat ID: `{}` ({})".format(event.chat_id,"Direct Message" if event.chat_id > 0 else "Group Chat"))
+        raise events.StopPropagation
+
